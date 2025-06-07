8c00e0cd0568a2fcad144c60e1e31937fa5fbc39
trongle <trongle1161@gmail.com>
2025-06-07 14:19:04 +0700

    login google

--- PerHue.Api/Program.cs
+++ PerHue.Api/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.OpenApi.Models;
+﻿using Microsoft.OpenApi.Models;
 using PerHue.Api.Utils;
 using PerHue.Application.Extensions;
 using PerHue.Infrastructure.Extensions;
@@ -15,32 +15,41 @@ builder.Services.Configure<RouteOptions>(options =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+builder.Services.AddSwaggerGen(c =>
 {
-	options.SwaggerDoc("v1", new OpenApiInfo { Title = "App.API", Version = "v1" });
-	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Google OAuth API", Version = "v1" });
+
+	// Tạo các định nghĩa cho OAuth2
+	c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
 	{
-		In = ParameterLocation.Header,
-		Description = "Please enter token  into field",
-		Name = "Authorization",
-		Type = SecuritySchemeType.Http,
-		BearerFormat = "JWT",
-		Scheme = "bearer",
-	});
-	options.AddSecurityRequirement(new OpenApiSecurityRequirement
-	{
+		Type = SecuritySchemeType.OAuth2,
+		Flows = new OpenApiOAuthFlows
 		{
-			new OpenApiSecurityScheme
+			Implicit = new OpenApiOAuthFlow
 			{
-				Reference = new OpenApiReference
-				{
-					Type = ReferenceType.SecurityScheme,
-					Id = "Bearer"
-				}
-			},
-			[]
+				AuthorizationUrl = new Uri("https://accounts.google.com/o/oauth2/auth"),
+				Scopes = new Dictionary<string, string>
+					{
+						{ "email", "Access your email" }
+					}
+			}
 		}
 	});
+
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+		{
+			{
+				new OpenApiSecurityScheme
+				{
+					Reference = new OpenApiReference
+					{
+						Type = ReferenceType.SecurityScheme,
+						Id = "oauth2"
+					}
+				},
+				new List<string> { "email" }
+			}
+		});
 });
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
@@ -62,7 +71,13 @@ await seeder.Seed();
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", "Google OAuth API V1");
+		c.OAuthClientId(builder.Configuration["Google:ClientId"]);  // Thêm Google Client ID của bạn
+		c.OAuthClientSecret(builder.Configuration["Google:ClientSecret"]);  // Thêm Google Client Secret của bạn
+		c.OAuthUsePkce();
+	});
 }
 
 app.UseCors();
