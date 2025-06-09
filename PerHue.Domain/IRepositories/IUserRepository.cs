6d48129754709e05894e1b1f149d66dec39dfa5a
trongle <trongle1161@gmail.com>
2025-06-09 14:11:43 +0700

    fix getall user

--- PerHue.Domain/IRepositories/IUserRepository.cs
+++ PerHue.Domain/IRepositories/IUserRepository.cs
@@ -6,6 +6,8 @@ namespace PerHue.Domain.IRepositories
 	public interface IUserRepository : IGenericRepository<UserAccount>
 	{
 		Task<bool> DeleteByEmailAsync(string email);
+		Task<IEnumerable<UserAccount>> GetAllAsync();
 		Task<UserAccount> GetByEmailAsync(string email);
+		Task<UserAccount> GetByIdAsync(int id);
 	}
 }
