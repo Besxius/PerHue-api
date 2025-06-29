ee5fd170c027382fd898253be2d4020c994d7b0c
trongle <trongle1161@gmail.com>
2025-06-29 23:37:20 +0700

    fix normal test

--- PerHue.Application/Models/CreateNormalTestResultModel.cs
+++ PerHue.Application/Models/CreateNormalTestResultModel.cs
@@ -4,7 +4,5 @@
 	{
 		public int UserId { get; set; }
 		public List<string> SelectedColors { get; set; }
-		public int ColorTypeId { get; set; }
-		public IEnumerable<CapsulePaletteModel> CapsulePalettes { get; set; } = null!;
 	}
 }
