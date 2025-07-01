3da5e6bf0a9a120234036cd58565068ca89177bf
trongle <trongle1161@gmail.com>
2025-07-02 00:12:33 +0700

    fix login

---
+++ PerHue.Infrastructure/AI/GeminiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerHue.Infrastructure.AI
+{
+	internal class GeminiService
+	{
+	}
+}
