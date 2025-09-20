6d0439f0196097db073846b66810b2427aca9ae1
trankien214 <kientse171441@fpt.edu.vn>
2025-09-20 09:54:48 +0700

    feat: otp mail sent to user to change password

---
+++ PerHue.Application/IServices/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerHue.Application.IServices;
+
+public interface IEmailService
+{
+	Task SendEmailAsync(string to, string subject, string body);
+
+}
