128a7e5a1ee6e6867b64521e50384a50551254e2
Besxius <trongle1161@gmail.com>
2025-09-20 22:53:46 +0700

    fix database structure

--- PerHue.Domain/Entities/UserAccount.cs
+++ PerHue.Domain/Entities/UserAccount.cs
@@ -45,6 +45,8 @@ public partial class UserAccount
 
     public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
 
+    public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+
     public virtual ICollection<Report> Reports { get; set; } = new List<Report>();
 
     public virtual Role Role { get; set; } = null!;
