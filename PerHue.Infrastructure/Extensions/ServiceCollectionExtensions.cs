57cd909a2d4bf854c2e62ea841d88974a01c370f
trankien214 <kientse171441@fpt.edu.vn>
2025-11-12 02:27:14 +0700

    feat/expert_test_flow_Unfinished

--- PerHue.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ PerHue.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -47,6 +47,13 @@ namespace PerHue.Infrastructure.Extensions
 			services.AddScoped<IVerificationRepository, VerificationRepository>();
 			services.AddScoped<IExpertRepository, ExpertRepository>();
 			services.AddScoped<INotificationRepository, NotificationRepository>();
+
+			services.AddScoped<ITestRequestRepository, TestRequestRepository>();
+			services.AddScoped<ITestResponseRepository, TestResponseRepository>();
+			services.AddScoped<IExpertTestRequestRepository, ExpertTestRequestRepository>();
+
+			services.AddScoped<IAiPictureRepository, AiPictureRepository>();
+			services.AddScoped<IAiTestResultRepository, AiTestResultRepository>();
 			#endregion
 
 			#region Services
@@ -64,6 +71,8 @@ namespace PerHue.Infrastructure.Extensions
 			services.AddScoped<IVerificationService, VerificationService>();
 			services.AddScoped<IExpertService, ExpertService>();
 			services.AddScoped<INotificationService, NotificationService>();
+
+			services.AddScoped<IExpertTestService, ExpertTestService>();
 			#endregion
 
 			#region Other Services
@@ -88,6 +97,8 @@ namespace PerHue.Infrastructure.Extensions
 			services.AddSignalR();
 			services.AddHostedService<AutoEmail>();
 			services.AddHostedService<PackageExpire>();
+
+			services.AddHostedService<ExpertTestMonitor>();
 			#endregion
 
 			#region Authentication	
---
+++ PerHue.Infrastructure/Repositories/AiPictureRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using PerHue.Domain.Entities;
+using PerHue.Domain.IRepositories;
+using PerHue.Infrastructure.Basic;
+using PerHue.Infrastructure.Persistence;
+
+namespace PerHue.Infrastructure.Repositories
+{
+	internal class AiPictureRepository : GenericRepository<AiPicture>, IAiPictureRepository
+	{
+		public AiPictureRepository(PerHueDbContext context) : base(context)
+		{
+		}
+
+		public async Task<IEnumerable<AiPicture>> GetPicturesForTestRequestAsync(int testRequestId)
+		{
+			return await _context.AiPictures
+				.Where(p => p.TestRequestId == testRequestId)
+				.ToListAsync();
+		}
+	}
+}
