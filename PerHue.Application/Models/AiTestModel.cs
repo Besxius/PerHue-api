c22f639224ad855f162da48b9eb2611e1975c041
NET_12_NguyenQuangKhanhAn <annqkhe180905@fpt.edu.vn>
2025-11-16 08:40:57 +0700

    before merge from Main

---
+++ PerHue.Application/Models/AiTestModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerHue.Application.Models
+{
+	public class AiTestModel
+	{
+		public class CreateAiTestRequestModel
+		{
+			public string? HairColor { get; set; }
+			public string? EyesColor { get; set; }
+			public string? LipsColor { get; set; }
+			public string? SkinColor { get; set; }
+			public List<IFormFile> Images { get; set; } = new();
+		}
+
+		public class AiTestResponseModel
+		{
+			public int TestRequestId { get; set; }
+			public string Status { get; set; }
+			public DateTime CreatedDate { get; set; }
+			public AiTestResultModel? Result { get; set; }
+		}
+
+		public class GeminiAnalysisRequest
+		{
+			public List<string> ImageUrls { get; set; } = new();
+			public string? HairColor { get; set; }
+			public string? EyesColor { get; set; }
+			public string? LipsColor { get; set; }
+			public string? SkinColor { get; set; }
+		}
+	}
+}
--- PerHue.Infrastructure/Migrations/20251113160829_dbV2.Designer.cs
+++ PerHue.Infrastructure/Migrations/20251113160829_dbV2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PerHue.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using PerHue.Infrastructure.Persistence;
 namespace PerHue.Infrastructure.Migrations
 {
     [DbContext(typeof(PerHueDbContext))]
-    partial class PerHueDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251113160829_dbV2")]
+    partial class dbV2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
