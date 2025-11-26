de61fc1e4ec677b1d698806cf1a9059e7cd1fdd4
Besxius <trongle1161@gmail.com>
2025-11-26 18:41:11 +0700

    update database

--- PerHue.Domain/Entities/TestResponse.cs
+++ PerHue.Domain/Entities/TestResponse.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 
 namespace PerHue.Domain.Entities;
 
-/// <summary>
-/// Lưu phản hồi/tư vấn từ chuyên gia về yêu cầu test, bao gồm ghi chú, 
-/// màu tốt nhất/tệ nhất, loại màu, đánh giá và thông tin chuyên gia.
-/// </summary>
 public partial class TestResponse
 {
     public int Id { get; set; }
