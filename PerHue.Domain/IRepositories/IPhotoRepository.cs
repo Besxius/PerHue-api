4169bb692748a0980d8eafd0290e1bfe156cb85e
NET_12_NguyenQuangKhanhAn <annqkhe180905@fpt.edu.vn>
2025-12-02 21:21:54 +0700

    fix: Ai Test

---
+++ PerHue.Domain/IRepositories/IPhotoRepository.cs
@@ -0,0 +1,10 @@
+﻿using PerHue.Domain.Basic;
+using PerHue.Domain.Entities;
+
+namespace PerHue.Domain.IRepositories
+{
+	public interface IPhotoRepository : IGenericRepository<Photo>
+	{
+		Task CreatePhotosAsync(List<Photo> photos);
+	}
+}
