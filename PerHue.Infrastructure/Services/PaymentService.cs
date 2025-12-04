52ffa69b9b8fbd17ef7ca3dbcd4ff56a36b1aa47
NET_12_NguyenQuangKhanhAn <annqkhe180905@fpt.edu.vn>
2025-12-04 19:41:11 +0700

    fix: Api Payment-Callback functions and return message, SubmitRequestHasPhotoModel,

--- PerHue.Infrastructure/Services/PaymentService.cs
+++ PerHue.Infrastructure/Services/PaymentService.cs
@@ -64,6 +64,7 @@ namespace PerHue.Infrastructure.Services
 		public async Task<int> CreateSuccessPaymentInDbAsync(PerHue.Application.Models.Payment.An.CreatePaymentModel model) {
 			var payment = new Payment
 			{
+				Id = model.PaymentId,
 				UserId = model.UserId,
 				Amount = model.Amount,
 				Description = model.Description,
