b10351ffdc841c53af2d89560a5de85769304406
dhung2103 <hunglupu@gmail.com>
2025-12-09 21:42:23 +0700

    Expand admin API access to include Moderator role
    Updated [Authorize] attributes on multiple admin controllers to allow both "Admin" and "Moderator" roles access. No other logic or functionality was changed.

--- PerHue.Api/Controllers/Admin/CapsulePaletteManagementController.cs
+++ PerHue.Api/Controllers/Admin/CapsulePaletteManagementController.cs
@@ -9,7 +9,7 @@ namespace PerHue.Api.Controllers.Admin
 {
 	[Route("api/admin/[controller]")]
 	[ApiController]
-	[Authorize(Roles = "Admin")]
+	[Authorize(Roles = "Admin, Moderator")]
 	public class CapsulePaletteManagementController : ControllerBase
 	{
 		private readonly IServicesProvider _servicesProvider;
