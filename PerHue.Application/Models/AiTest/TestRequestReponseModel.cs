8ce9fff6e66bd16092dd65743706f6ec0a5b320d
NET_12_NguyenQuangKhanhAn <annqkhe180905@fpt.edu.vn>
2025-12-09 23:05:09 +0700

    fix: get Ai Test history, get Ai Test by ID, GetMyManualTests sort by date

---
+++ PerHue.Application/Models/AiTest/TestRequestReponseModel.cs
@@ -0,0 +1,56 @@
+﻿using PerHue.Application.Models.CapsulePalette;
+using PerHue.Application.Models.Color;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PerHue.Application.Models.AiTest
+{
+	public class NewTestRequestReponseModel
+	{
+		public int Id { get; set; }
+
+		public string? HairColor { get; set; }
+
+		public string? EyesColor { get; set; }
+
+		public string? LipsColor { get; set; }
+
+		public string? SkinColor { get; set; }
+
+		public string? Status { get; set; }
+
+		public DateTime? CreatedDate { get; set; }
+
+		public string TypeOfTest { get; set; } = null!;
+
+		public string? Fullname { get; set; }
+
+		public string? ImageUrl { get; set; }
+
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+		public NewAiTestResultResponseModel? newAiTestResultResponseModel { get; set; }
+
+
+	}
+
+	public class NewAiTestResultResponseModel
+	{
+		public int Id { get; set; }
+
+		public string? Note { get; set; }
+
+		public string SuggestedColor { get; set; } = null!;
+
+		public string AvoidedColor { get; set; } = null!;
+
+		public int ColorTypeId { get; set; }
+
+		public List<ColorModel> SuggestedColorsBySystem { get; set; } = new List<ColorModel>();
+
+		public CapsulePaletteModel SuggestedCapsulePalleteBySystem { get; set; } = new CapsulePaletteModel();
+	}
+}
