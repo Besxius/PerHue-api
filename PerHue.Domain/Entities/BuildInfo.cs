a1ab3f05627b854dcbeb6386f257ac354cc6af35
dhung2103 <hunglupu@gmail.com>
2025-12-14 21:59:26 +0700

    refactor BuildInfo properties to be nullable and add GetShortCommit method for better commit handling

--- PerHue.Domain/Entities/BuildInfo.cs
+++ PerHue.Domain/Entities/BuildInfo.cs
@@ -8,9 +8,19 @@ namespace PerHue.Domain.Entities
 {
 	public class BuildInfo
 	{
-		public DateTime BuildTime { get; set; }
-		public string Commit { get; set; } = "";
-		public string Branch { get; set; } = "";
+		public DateTime? BuildTime { get; set; }
+		public string? Commit { get; set; } = "";
+		public string? Branch { get; set; } = "";
 		public int RunId { get; set; }
+
+		public string GetShortCommit()
+		{
+			if (string.IsNullOrWhiteSpace(Commit))
+				return "unknown";
+
+			return Commit.Length <= 7
+				? Commit
+				: Commit[..7];
+		}
 	}
 }
