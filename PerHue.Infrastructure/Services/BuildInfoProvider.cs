8b2198a6ebc297aadbab33cfb98727052a5c9499
dhung2103 <hunglupu@gmail.com>
2025-12-14 21:54:54 +0700

    add build info tracking and expose via API

---
+++ PerHue.Infrastructure/Services/BuildInfoProvider.cs
@@ -0,0 +1,22 @@
+﻿using PerHue.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PerHue.Infrastructure.Services
+{
+	public static class BuildInfoProvider
+	{
+		public static BuildInfo? Load()
+		{
+			var path = Path.Combine(AppContext.BaseDirectory, "buildinfo.json");
+			if (!File.Exists(path)) return null;
+
+			var json = File.ReadAllText(path);
+			return JsonSerializer.Deserialize<BuildInfo>(json);
+		}
+	}
+}
