74193235e51d59b7aea536125085dd1420ce3bf1
dhung2103 <hunglupu@gmail.com>
2025-12-26 21:23:14 +0700

    Make UpdateAsync virtual; override in ServicePackageRepo
    Made UpdateAsync virtual in GenericRepository to allow overrides. Overrode UpdateAsync in ServicePackageRepository to prevent updating the Uses property. Added XML docs to the overridden method.

--- PerHue.Infrastructure/Repositories/ServicePackageRepository.cs
+++ PerHue.Infrastructure/Repositories/ServicePackageRepository.cs
@@ -16,5 +16,23 @@ namespace PerHue.Infrastructure.Repositories
 		{
 			return await _context.ServicePackages.FirstOrDefaultAsync(sp => sp.Price == amount);
 		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <returns></returns>
+		public override async Task<int> UpdateAsync(ServicePackage entity)
+		{
+			_context.ChangeTracker.Clear();
+
+			var entry = _context.Attach(entity);
+			entry.State = EntityState.Modified;
+
+			entry.Property(x => x.Uses).IsModified = false;
+
+			return await _context.SaveChangesAsync();
+		}
+
 	}
 }
